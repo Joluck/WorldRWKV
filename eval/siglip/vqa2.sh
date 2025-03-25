3c1a389e7eb8c32b89d34ae75ee7b2e40c119b9e
Ojiyumm <125381866+Ojiyumm@users.noreply.github.com>
2025-03-25 15:44:16 +0800

    Update vqa2.sh
    fix benchmark scripts

--- eval/siglip/vqa2.sh
+++ eval/siglip/vqa2.sh
@@ -1,16 +1,19 @@
-answer_file=/home/rwkvos/data/eval/vqav2/answers/rwkv7-3b-siglip
-mod="google/siglip2-base-patch16-384"
+answer_file=/mnt/program/WorldRWKV/Visual/jl_data/eval/vqav2/answers/rwkv7-3b-siglip
+mod="/home/rwkvos/models/siglip2"
 type=siglip
+base_dir=/mnt/program/WorldRWKV/Visual/jl_data/eval/vqav2 
+
+
 python -m eval.vqa2 \
-    --model-path /home/rwkvos/JL/out_model/rwkv7-3b-siglip/rwkv-0 \
-    --question-file /home/rwkvos/data/eval/vqav2/llava_vqav2_mscoco_test-dev2015.jsonl \
-    --image-folder /home/rwkvos/data/eval/vqav2/test2015 \
+    --model-path /home/rwkvos/world/out_model/rwkv7-3b-conv/step2/rwkv-0 \
+    --question-file $base_dir/llava_vqav2_mscoco_test-dev2015.jsonl \
+    --image-folder $base_dir/test2015 \
     --answers-file $answer_file/merge.jsonl \
     --conv_mode $mod \
     --type $type \
     --temperature 0
 
 python -m eval.eval_vqav2 \
-    --dir /home/rwkvos/data/eval/vqav2 \
+    --dir $base_dir \
     --split llava_vqav2_mscoco_test-dev2015 \
     --ckpt $answer_file
