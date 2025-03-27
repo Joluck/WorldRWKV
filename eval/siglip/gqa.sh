f2787fce25b7c079185a778a9c4bbe272ebf8071
Ojiyumm <yzydoomsday@gmail.com>
2025-03-27 15:09:28 +0800

    add pope and vizwiz benchmark. add visual training scripts
    add pope and vizwiz benchmark. add visual training scripts

--- eval/siglip/gqa.sh
+++ eval/siglip/gqa.sh
@@ -1,11 +1,11 @@
-answer_file=/mnt/program/WorldRWKV/Visual/jl_data/eval/gqa/answers/rwkv7-3b-conv
+answer_file=/mnt/program/WorldRWKV/Visual/jl_data/eval/gqa/answers/rwkv7-0.4b-g1-siglip
 mod="/home/rwkvos/models/siglip2"
 type=siglip
 base_dir=/mnt/program/WorldRWKV/Visual/jl_data/eval/gqa 
 
 
 python -m eval.vqa2 \
-    --model-path /home/rwkvos/world/out_model/rwkv7-3b-conv/step2/rwkv-0  \
+    --model-path /home/rwkvos/WorldRWKV/out_model/rwkv7-g1-0.4b/step2/rwkv-0  \
     --question-file $base_dir/llava_gqa_testdev_balanced.jsonl \
     --image-folder $base_dir/images \
     --answers-file $answer_file/merge.jsonl \
